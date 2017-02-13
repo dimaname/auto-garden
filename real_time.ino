066acd3eb6a34da2a5856626d622ed4480d11250
tmp <tmp@DESKTOP-OLBGNGB>
2017-02-14 02:23:17 +0500

    Добавьте файлы проекта.

---
+++ real_time.ino
@@ -0,0 +1,67 @@
+#include "z.h"
+#include <TroykaRTC.h>
+#include <TimerOne.h>
+#include <Thread.h>
+#include "Schedule.h"
+
+Thread timeThread = Thread(); 
+Thread printThread = Thread();
+
+
+
+
+
+// создаём объект для работы с часами реального времени
+RTC clock;
+Schedule schedule(clock);
+
+
+
+void printTime() {
+	// выводим в serial порт текущее время, дату и день недели
+	Serial.print(schedule.time);
+	Serial.print(" -- ");
+	Serial.print(schedule.date);
+	Serial.print(" -- ");
+	Serial.println(schedule.weekday);
+
+}
+
+void timer1_action() {		
+	schedule.tact();
+}
+
+
+
+void setup()
+{
+
+//	timeThread.onRun( getTime );
+//  timeThread.setInterval(90);
+
+	printThread.onRun( printTime );
+	printThread.setInterval(1000);
+	// открываем последовательный порт
+	Serial.begin(9600);
+	// инициализация часов
+	clock.begin();
+	// метод установки времени и даты в модуль вручную
+	// clock.set(10,25,45,27,07,2005,THURSDAY);
+	// метод установки времени и даты автоматически при компиляции
+	clock.set(__TIMESTAMP__);
+
+
+	Timer1.initialize(10000);
+	Timer1.attachInterrupt(timer1_action);
+}
+
+void loop()
+{
+	
+				
+	if (printThread.shouldRun())
+		printThread.run(); // запускаем поток
+
+
+
+}
---
+++ schedule.h
@@ -0,0 +1,56 @@
+﻿// schedule.h
+#ifndef _SCHEDULE_h
+#define _SCHEDULE_h
+
+// размер массива для времени с учётом завершающего нуля
+#define LEN_TIME 12
+// размер массива для даты с учётом завершающего нуля
+#define LEN_DATE 12
+// размер массива для дня недели с учётом завершающего нуля
+#define LEN_DOW 12
+#include <Wire.h>
+// библиотека для работы с часами реального времени
+#include "TroykaRTC.h"
+
+// EEPROM — энергонезависимая память
+// библиотека для записи и считывания информации с EEPROM
+#include <EEPROM.h>
+
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+
+
+
+class Schedule {
+private:
+	class ScheduleItem {
+	public:
+		int type;
+		ScheduleItem() {
+			type = Schedule::TYPE::EveryDay;
+		}
+
+	};
+
+public:
+	Schedule(RTC);
+	enum TYPE { EveryDay, EveryWeek };
+	char time[LEN_TIME] = { 0 };	
+	char date[LEN_DATE] = { 0 };	
+	char weekday[LEN_DOW] = { 0 };
+
+	void tact();
+
+
+
+}; 
+
+#endif
+
+
