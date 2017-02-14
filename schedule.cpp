cb598cce6ab6907264baa50eee6e3ce775b1e659
tmp <tmp@DESKTOP-OLBGNGB>
2017-02-14 20:52:22 +0500

    расписание проверяет день

--- schedule.cpp
+++ schedule.cpp
@@ -8,32 +8,96 @@
 		clock = _clock;	
 	};
 
+	int Schedule::weekday = 0;
 
 	void Schedule::tact() {
 		sei();	
 		clock.read();
-		clock.getTimeStr(time, LEN_TIME);
+		clock.getTimeStr(time, LEN_TIME);			
 		clock.getDateStr(date, LEN_DATE);	
-		clock.getDOWStr(weekday, LEN_DOW);
+		Schedule::weekday = clock.getDOW();
+		checkTasks();		
 		cli();
-
 	};
 
-	void Schedule::addTask( String timestamp, void callback()) {	
+	void Schedule::checkTasks() {
 		
+		for (std::vector<ScheduleItem>::iterator it = items.begin(); it != items.end(); ++it)
+		{
+			ScheduleItem & tmp = *it;
 		
-		Schedule::TYPE type = Schedule::TYPE::EveryDay;
-		callback();
-		switch (type) {
-		case Schedule::TYPE::EveryDay:
-			Serial.print(" EveryDay ... ");
-			items.push_back(ScheduleItem());
-			break;
-		case Schedule::TYPE::EveryWeek:
-			Serial.print(" EveryWeek ... ");
-			items.push_back(ScheduleItem());
-			break;
-		default:
-			Serial.print(" takoe ... ");
-		};
+				if (tmp.canFire()) {
+					tmp.fire();
+				}
+			
+			
+		}
+
+	
 	};
+
+	void Schedule::addTask( String timeplan, void callback()) {	
+		
+		
+		Schedule::TYPE type;
+
+		timeplan.trim();
+		int comPos = timeplan.indexOf(",");
+		String weekDayPart, timePart;
+		vector<int> daysOfWeekVector;
+		if (comPos == -1) {
+			type = Schedule::TYPE::EveryDay;
+			timePart = timeplan;		
+		}
+		else {
+			type = Schedule::TYPE::EveryWeek;
+			weekDayPart = timeplan.substring(0, comPos);
+			timePart = timeplan.substring(comPos+1);
+			weekDayPart.trim();
+			daysOfWeekVector = daysToArray(weekDayPart);		
+			timePart.trim();
+		}
+		
+		//Serial.println("weekDayPart " + weekDayPart);
+		//Serial.println("timePart " + timePart);
+	 	items.push_back(ScheduleItem(type, daysOfWeekVector, callback));
+	};
+
+	vector<int> Schedule::daysToArray(String str) {
+		int startPos = 0;
+		int spacePos = str.indexOf(" ", startPos);
+		String day;
+		vector<int> ret;
+		while (spacePos != -1) {		
+		
+			ret.push_back(nameOfDayToNumber(str.substring(startPos, spacePos)));
+			startPos = spacePos+1;
+			spacePos = str.indexOf(" ", startPos);
+		}		
+		ret.push_back(nameOfDayToNumber(str.substring(startPos)));
+		return ret;
+	}
+
+	int  Schedule::nameOfDayToNumber(String day) {
+		day.toUpperCase();
+		if (day == "MO") {
+			return 1;				
+		}else if(day == "TU") {
+			return 2;
+		}else if (day == "WE") {
+			return 3;
+		}
+		else if (day == "TH") {
+			return 4;
+		}
+		else if (day == "FR") {
+			return 5;
+		}
+		else if (day == "SA") {
+			return 6;
+		}
+		else if (day == "SU") {
+			return 7;
+		}	
+		return -1;
+	}
--- schedule.h
+++ schedule.h
@@ -6,13 +6,13 @@
 #define LEN_TIME 12
 // размер массива для даты с учётом завершающего нуля
 #define LEN_DATE 12
-// размер массива для дня недели с учётом завершающего нуля
-#define LEN_DOW 12
+
 #include <Wire.h>
 #include <EEPROM.h>
 #include <TroykaRTC.h>
 #include <StandardCplusplus.h>
 #include <vector>
+#include <algorithm>
 #if defined(ARDUINO) && ARDUINO >= 100
 #include "arduino.h"
 #else
@@ -21,32 +21,72 @@
 
 using namespace std;
 class Schedule {
+public:
+	enum TYPE { EveryDay, EveryWeek };
 private:
+
+
 	class ScheduleItem {
+	private:
+		int prevFirePeriodWas;
+		void(*callback)();
+
 	public:
-		int type;
-		ScheduleItem() {
-			type = Schedule::TYPE::EveryDay;
+		vector<int> weekdays;
+		Schedule::TYPE type;
+
+		ScheduleItem(Schedule::TYPE _type, vector<int> _weekdays, void(*_callback)()) {
+			type = _type;
+			callback = _callback;
+			weekdays = _weekdays;
+			prevFirePeriodWas = -1;
+		}
+
+		void fire() {
+			this->callback();
+		}
+
+		bool canFire() {
+			bool isCanFire = false;
+
+			switch (type)
+			{
+			case Schedule::EveryDay:
+				isCanFire = prevFirePeriodWas != Schedule::weekday;
+				this->prevFirePeriodWas = Schedule::weekday;
+				break;
+			case Schedule::EveryWeek:
+				if (find(this->weekdays.begin(), this->weekdays.end(), Schedule::weekday) != this->weekdays.end()) {
+					isCanFire = prevFirePeriodWas != Schedule::weekday;
+					this->prevFirePeriodWas = Schedule::weekday;
+				}
+				break;
+			default:
+				break;
+			}
+
+			return isCanFire;
 		}
 
 	};
-	vector<ScheduleItem> items;
-
 public:
 	RTC clock;
-
 	Schedule(RTC);
-	enum TYPE { EveryDay, EveryWeek };
+	vector<int> daysToArray(String);
+	int nameOfDayToNumber(String);
 	char time[LEN_TIME] = { 0 };
 	char date[LEN_DATE] = { 0 };
-	char weekday[LEN_DOW] = { 0 };
+	static int weekday;
 
 	void tact();
 	void addTask(String, void());
-
-
+	void Schedule::checkTasks();
+	vector<ScheduleItem> items;
 };
 
+
 #endif
+
+
 
 
